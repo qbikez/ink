7841ef5254c4e38341cdc72d0d4a212290283202
jakub.pawlowski <jakub.pawlowski@live.com>
2025-05-03 11:08:05 +0200

    render options

--- build/render.d.ts
+++ build/render.d.ts
@@ -1,48 +1,6 @@
 import type { ReactNode } from "react";
-import Ink from "./ink.js";
-export type RenderOptions = {
-    /**
-     * Output stream where app will be rendered.
-     *
-     * @default process.stdout
-     */
-    stdout?: NodeJS.WriteStream;
-    /**
-     * Input stream where app will listen for input.
-     *
-     * @default process.stdin
-     */
-    stdin?: NodeJS.ReadStream;
-    /**
-     * Error stream.
-     * @default process.stderr
-     */
-    stderr?: NodeJS.WriteStream;
-    /**
-     * If true, each update will be rendered as a separate output, without replacing the previous one.
-     *
-     * @default false
-     */
-    debug?: boolean;
-    /**
-     * Configure whether Ink should listen to Ctrl+C keyboard input and exit the app. This is needed in case `process.stdin` is in raw mode, because then Ctrl+C is ignored by default and process is expected to handle it manually.
-     *
-     * @default true
-     */
-    exitOnCtrlC?: boolean;
-    /**
-     * Patch console methods to ensure console output doesn't mix with Ink output.
-     *
-     * @default true
-     */
-    patchConsole?: boolean;
-    /**
-     * Throttle renders to at most once every *throttleMs*
-     *
-     * @default 8
-     */
-    throttle?: number;
-};
+import Ink, { type Options as InkOptions } from "./ink.js";
+export type RenderOptions = Partial<InkOptions>;
 export type Instance = {
     /**
      * Replace previous root node with a new one or update props of the current root node.
